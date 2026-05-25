9acc8d83b13a872e1e3cb8b4f50cae7fdfa4b74f
Shivam Gupta <shivam.gupta19112003@gmail.com>
2026-05-25 15:02:27 +0530

    Adding OTP Verification

--- src/helpers/sendVerificatinEmail.ts
+++ src/helpers/sendVerificatinEmail.ts
@@ -1,22 +1,44 @@
-import { resend } from "../../lib/resend";
-import VerificationEmail from "../../emails/VerificationEmail";
-import { ApiResponse } from '@/types/ApiResponse';
+import nodemailer from "nodemailer";
 
 export async function sendVerificationEmail(
   email: string,
   username: string,
   verifyCode: string
-): Promise<ApiResponse> {
+) {
   try {
-    await resend.emails.send({
-      from: "onboarding@resend.dev",
-      to: email,
-      subject: 'Mystery Message Verification Code',
-      react: VerificationEmail({ username, otp: verifyCode }),
+    const transporter = nodemailer.createTransport({
+      host: "smtp-relay.brevo.com",
+      port: 587,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
     });
-    return { success: true, message: 'Verification email sent successfully.' };
-  } catch (emailError) {
-    console.error('Error sending verification email:', emailError);
-    return { success: false, message: 'Failed to send verification email.' };
+
+    await transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to: email,
+      subject: "Mystery Message Verification Code",
+      html: `
+        <div style="font-family:sans-serif;">
+          <h2>Hello ${username}</h2>
+          <p>Your verification code is:</p>
+          <h1>${verifyCode}</h1>
+          <p>This code expires in 1 hour.</p>
+        </div>
+      `,
+    });
+
+    return {
+      success: true,
+      message: "Verification email sent successfully.",
+    };
+  } catch (error) {
+    console.error("Error sending email:", error);
+
+    return {
+      success: false,
+      message: "Failed to send verification email.",
+    };
   }
 }
